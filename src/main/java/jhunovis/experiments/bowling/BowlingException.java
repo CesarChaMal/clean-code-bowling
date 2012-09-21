4ea9404d8da10c24dcbaf639c7d2dfa68f7913ee
Jan Hackel <jan@hackel.name>
2012-09-21 15:11:43 +0200

    Remove field. They are not needed.

--- src/main/java/jhunovis/experiments/bowling/BowlingException.java
+++ src/main/java/jhunovis/experiments/bowling/BowlingException.java
@@ -1,17 +1,14 @@
 package jhunovis.experiments.bowling;
 
+import java.util.Arrays;
+
 @SuppressWarnings("serial")
 public class BowlingException extends RuntimeException {
 
 	private static final String MESSGAGE 
 		= "Cannot add roll %d to current game with rolls %s";
 
-	public int[] currentGameRolls;
-	public int roll;
-
 	public BowlingException(int roll, int[] currentGameRolls) {
-		super(String.format(MESSGAGE, roll, currentGameRolls));
-		this.roll = roll;
-		this.currentGameRolls = currentGameRolls;
+		super(String.format(MESSGAGE, roll, Arrays.toString(currentGameRolls)));
 	}
 }
